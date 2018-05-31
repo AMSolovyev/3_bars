a86e50be77bc988b84f1d65edf2b00e14382045e
AMSolovyev <alexander.solovyev7@gmail.com>
2018-05-31 21:36:19 +0300

    I have renamed some functions and have added print-function

--- bars.py
+++ bars.py
@@ -7,41 +7,58 @@ import sys
 def load_data_from_json(file_path):
     try:
         with open(file_path, 'r') as json_file:
-            decoded_json_file = json.load(json_file)
-            json_bars = decoded_json_file['features']
+            decoded_json = json.load(json_file)
+            json_bars = decoded_json['features']
             return json_bars
     except json.decoder.JSONDecodeError:
         return None
 
 
-def get_biggest_bar(json_bars):
+def get_biggest_bar(bars):
     biggest_bar = max(
-        json_bars, key=lambda x: x['properties']['Attributes']['SeatsCount'])
+        bars, key=lambda x: x['properties']['Attributes']['SeatsCount'])
     return biggest_bar
 
 
-def get_smallest_bar(json_bars):
+def get_smallest_bar(bars):
     smallest_bar = min(
-        json_bars, key=lambda x: x['properties']['Attributes']['SeatsCount'])
+        bars, key=lambda x: x['properties']['Attributes']['SeatsCount'])
     return smallest_bar
 
 
-def get_closest_bar(json_bars, longitude, latitude):
+def get_closest_bar(bars, longitude, latitude):
     distance = min(
-            json_bars, key=lambda bar: (
-                    (bar['geometry']['coordinates'][0] - latitude)**2 -
-                    (bar['geometry']['coordinates'][1] - longitude)**2))
+        bars, key=lambda bar: (
+                (bar['geometry']['coordinates'][0] - latitude)**2 -
+                (bar['geometry']['coordinates'][1] - longitude)**2))
 
     return distance
 
 
-def enter_coordinate():
+def get_coordinates():
     try:
         latitude = float(input('Input coordinate latitude:'))
         longitude = float(input('Input coordinate longitude:'))
         return longitude, latitude
     except ValueError:
         return None
+
+
+def print_bars():
+    bars = load_data_from_json(file_path)
+    biggest_bar = get_biggest_bar(bars)
+    print(
+        'The biggest bar: {}'.format(
+            biggest_bar['properties']['Attributes']['Name']))
+    smallest_bar = get_smallest_bar(bars)
+    print(
+        'The smallest bar: {}'.format(
+            smallest_bar['properties']['Attributes']['Name']))
+    latitude, longitude = get_coordinates()
+    closest_bar = get_closest_bar(bars, longitude, latitude)
+    print(
+        'The closest bar: {}'.format(
+            closest_bar['properties']['Attributes']['Name']))
 
 
 if __name__ == '__main__':
@@ -51,22 +68,4 @@ if __name__ == '__main__':
     if not os.path.isfile(file_path):
         exit('There is not any file like this')
 
-    json_bars = load_data_from_json(file_path)
-
-    biggest_bar = get_biggest_bar(json_bars)
-    print(
-        'The biggest bar: {}'.format(
-            biggest_bar['properties']['Attributes']['Name']))
-
-    smallest_bar = get_smallest_bar(json_bars)
-    print(
-        'The smallest bar: {}'.format(
-            smallest_bar['properties']['Attributes']['Name']))
-
-    latitude, longitude = enter_coordinate()
-
-    print(
-        'The closest bar: {}'.format(
-            get_closest_bar(
-                json_bars, latitude,
-                longitude)['properties']['Attributes']['Name']))
+    print_bars()
